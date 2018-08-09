1fbb0bcdfb3767257ce76bab5a8ea6b589c4ae74
Ethan Flood <ethan.flood@gmail.com>
2018-08-09 14:25:25 -0400

    recenter menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,15 +5,15 @@ class Menu {
   }
   draw() {
     this.context.beginPath();
-    this.context.fillText("Get a Job", 400, 200);
+    this.context.fillText("Get a Job", 435, 200);
     this.context.fillStyle = "#ff0000";
     this.context.closePath();
     this.context.beginPath();
-    this.context.fillText("IN SPACE", 395, 300);
+    this.context.fillText("IN SPACE", 430, 300);
     this.context.fillStyle = "#ff0000";
     this.context.closePath();
     this.context.beginPath();
-    this.context.fillText("Press Space to Start the Hunt", 300, 400);
+    this.context.fillText("Press Space to Start the Hunt", 350, 400);
     this.context.fillStyle = "#ff0000";
     this.context.closePath();
   }
