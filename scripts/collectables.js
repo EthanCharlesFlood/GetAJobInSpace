00b0da507e059025523b59ecd4e639928371910b
Ethan Flood <ethan.flood@gmail.com>
2018-12-17 23:11:15 -0500

    add attributes to collectable

--- scripts/collectables.js
+++ scripts/collectables.js
@@ -1,5 +1,25 @@
 import GameObject from './object.js';
 
 class Collectable extends GameObject {
-  
+  constructor(canvasWidth, canvasHeight, ctx) {
+    super(canvasWidth, canvasHeight);
+    this.context = ctx;
+    this.collected = false;
+    this.dx = 10;
+  }
+
+  hitbox() {
+    return {
+      x1: this.x,
+      x2: this.x + 50,
+      y1: this.y + 15,
+      y2: this.y + 50,
+    };
+  }
+
+  draw() {
+
+  }
 }
+
+export default Collectable;
