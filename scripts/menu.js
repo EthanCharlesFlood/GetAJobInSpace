cfd070f24e2535aa134aade788ffd23c151bc4ca
Ethan Flood <ethan.flood@gmail.com>
2019-01-21 00:19:21 -0500

    work on tutorial

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,7 +2,25 @@ class Menu {
   constructor(context) {
     this.context = context;
     this.draw = this.draw.bind(this);
+    this.selector = 0;
   }
+
+  up() {
+    if (this.selector === 0) {
+      this.selector = 1;
+    } else if (this.selector === 1) {
+      this.selector = 0;
+    }
+  }
+
+  down() {
+    if (this.selector === 0) {
+      this.selector = 1;
+    } else if (this.selector === 1) {
+      this.selector = 0;
+    }
+  }
+
   draw() {
     this.context.beginPath();
     this.context.fillText("Get a Job", 435, 200);
@@ -16,6 +34,17 @@ class Menu {
     this.context.fillText("Press Space to Start the Hunt", 330, 400);
     this.context.fillStyle = "#ff0000";
     this.context.closePath();
+    if (this.selector === 0) {
+      this.context.beginPath();
+      this.context.fillText("PLAY", 430, 300);
+      this.context.fillStyle = "#ff0000";
+      this.context.closePath();
+    } else if (this.selector === 1) {
+      this.context.beginPath();
+      this.context.fillText("TUTORIAL", 430, 300);
+      this.context.fillStyle = "#ff0000";
+      this.context.closePath();
+    }
   }
 }
 
--- scripts/tutorial.js
+++ scripts/tutorial.js
@@ -3,45 +3,71 @@ class Tutorial {
     this.context = context;
     this.draw = this.draw.bind(this);
     this.counter = 0;
+    this.done = false;
+    this.enemy = enemy;
+    this.collectable = collectable;
   }
 
   left() {
-
+    this.counter += 1;
   }
 
   right() {
-
+    this.counter += 1;
   }
 
   up() {
-
+    this.counter += 1;
   }
 
   down() {
-
+    this.counter += 1;
   }
 
   demoEnemy() {
-
+    this.enemy.draw();
   }
 
   demoCollect() {
-    
+    this.collectable.draw();
   }
 
   draw() {
-    this.context.beginPath();
-    this.context.fillText("Get a Job", 435, 200);
-    this.context.fillStyle = "#ff0000";
-    this.context.closePath();
-    this.context.beginPath();
-    this.context.fillText("IN SPACE", 430, 300);
-    this.context.fillStyle = "#ff0000";
-    this.context.closePath();
-    this.context.beginPath();
-    this.context.fillText("Press Space to Start the Hunt", 330, 400);
-    this.context.fillStyle = "#ff0000";
-    this.context.closePath();
+    if (this.counter < 15) {
+      this.context.beginPath();
+      this.context.fillText("Press left to move left.", 330, 400);
+      this.context.fillStyle = "#ff0000";
+      this.context.closePath();
+    } else if (this.counter < 30) {
+      this.context.beginPath();
+      this.context.fillText("Press right to move right.", 330, 400);
+      this.context.fillStyle = "#ff0000";
+      this.context.closePath();
+    } else if (this.counter < 45) {
+      this.context.beginPath();
+      this.context.fillText("Press up to move up", 330, 400);
+      this.context.fillStyle = "#ff0000";
+      this.context.closePath();
+    } else if (this.counter < 60) {
+      this.context.beginPath();
+      this.context.fillText("Press down to move down.", 330, 400);
+      this.context.fillStyle = "#ff0000";
+      this.context.closePath();
+    } else if (this.counter < 75) {
+      this.context.beginPath();
+      this.context.fillText("Dodge enemies and obstacles.", 330, 400);
+      this.context.fillStyle = "#ff0000";
+      this.context.closePath();
+      this.demoEnemy();
+    } else if (this.counter < 100) {
+      this.context.beginPath();
+      this.context.fillText("Collect opportunities as they appear.", 330, 400);
+      this.context.fillStyle = "#ff0000";
+      this.context.closePath();
+      this.demoCollect();
+    } else {
+      this.done = true;
+    }
   }
 }
 
