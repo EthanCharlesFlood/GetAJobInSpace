1fea1fbcada7319ccdc589409e935497c755837f
Ethan Flood <ethan.flood@gmail.com>
2019-02-18 23:22:22 -0500

    add instance variable

--- scripts/key_handler.js
+++ scripts/key_handler.js
@@ -1,10 +1,11 @@
 class KeyHandler {
-  constructor(e) {
+  constructor(e, menu) {
     this.upPressed = false;
     this.downPressed = false;
     this.leftPressed = false;
     this.rightPressed = false;
     this.e = e;
+    this.menu = menu;
   }
 
   keyDownHandler() {
